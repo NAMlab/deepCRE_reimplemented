3d9511ffc02fdb65b0d52dab996c9e70d1bc78cc
Gernot Schmitz <spammail1240@web.de>
2024-08-30 17:04:56 +0200

    removed all left over functionality from utils.py. finished work on interpre and predict

--- deepcre_interprete.py
+++ deepcre_interprete.py
@@ -1,36 +1,176 @@
 import argparse
+import os
+from typing import List
 import pandas as pd
-from utils import extract_scores
 import tensorflow as tf
-tf.compat.v1.disable_eager_execution()
-tf.compat.v1.disable_v2_behavior()
-tf.config.set_visible_devices([], 'GPU')
+import h5py
+import numpy as np
+from deeplift.dinuc_shuffle import dinuc_shuffle
+import shap
 
-parser = argparse.ArgumentParser(
-                    prog='deepCRE',
-                    description="""
-                    This script performs the deepCRE prediction. We assume you have the following three directories:
-                    tmp_counts (contains your counts files), genome (contains the genome fasta files),
-                    gene_models (contains the gtf files)
-                    """)
-
-parser.add_argument('--input',
-                    help="""
-                    This is a 5 column csv file with entries: genome, gtf, tpm, output name, number of chromosomes.""",
-                    required=True)
-parser.add_argument('--model_case', help="Can be SSC or SSR", required=True)
-parser.add_argument('--ignore_small_genes', help="Ignore small genes, can be yes or no", required=True)
-
-args = parser.parse_args()
-data = pd.read_csv(args.input, sep=',', header=None,
-                   dtype={0: str, 1: str, 2: str, 3: str, 4: str},
-                   names=['genome', 'gtf', 'tpm', 'output', 'chroms'])
-print(data.head())
-if data.shape[1] != 5:
-    raise Exception("Input file incorrect. Your input file must contain 5 columns and must be .csv")
+from utils import get_time_stamp, get_filename_from_path, load_input_files, make_absolute_path
+from deepcre_predict import predict_self
+from train_ssr_models import extract_genes
 
 
-for genome, gtf, tpm_counts, output_name, num_chromosomes in data.values:
-    extract_scores(genome=genome, annot=gtf, tpm_targets=tpm_counts, upstream=1000, downstream=500,
-                   n_chromosome=num_chromosomes, ignore_small_genes=args.ignore_small_genes,
-                   output_name=output_name, model_case=args.model_case)
+# 1. Shap
+def dinuc_shuffle_several_times(list_containing_input_modes_for_an_example, seed=1234):
+    assert len(list_containing_input_modes_for_an_example) == 1
+    onehot_seq = list_containing_input_modes_for_an_example[0]
+    rng = np.random.RandomState(seed)
+    to_return = np.array([dinuc_shuffle(onehot_seq, rng=rng) for i in range(50)])
+
+    return [to_return]
+
+
+def combine_mult_and_diffref(mult, orig_inp, bg_data):
+    to_return = []
+    for l in range(len(mult)):
+        projected_hypothetical_contribs = np.zeros_like(bg_data[l]).astype("float")
+        assert len(orig_inp[l].shape) == 2
+        for i in range(orig_inp[l].shape[-1]):
+            hypothetical_input = np.zeros_like(orig_inp[l]).astype("float")
+            hypothetical_input[:, i] = 1.0
+            hypothetical_difference_from_reference = (hypothetical_input[None, :, :] - bg_data[l])
+            hypothetical_contribs = hypothetical_difference_from_reference * mult[l]
+            projected_hypothetical_contribs[:, :, i] = np.sum(hypothetical_contribs, axis=-1)
+        to_return.append(np.mean(projected_hypothetical_contribs, axis=0))
+    return to_return
+
+
+def compute_actual_hypothetical_scores(x, model):
+    """
+    This function computes the actual hypothetical scores given a model.
+
+    :param x: onehot encodings of correctly predicted sequences
+    :param model: loaded keras model used for predictions
+    :return:
+    """
+    shap.explainers.deep.deep_tf.op_handlers["AddV2"] = shap.explainers.deep.deep_tf.passthrough #type:ignore
+    shap.explainers.deep.deep_tf.op_handlers["FusedBatchNormV3"] = shap.explainers.deep.deep_tf.linearity_1d(0)#type:ignore
+    dinuc_shuff_explainer = shap.DeepExplainer(
+        (model.input, model.layers[-2].output[:, 0]),
+        data=dinuc_shuffle_several_times,
+        combine_mult_and_diffref=combine_mult_and_diffref)
+    hypothetical_scores = dinuc_shuff_explainer.shap_values(x)
+    actual_scores = hypothetical_scores * x
+    return actual_scores, hypothetical_scores
+
+
+def extract_scores(genome_file_name, annotation_file_name, tpm_counts_file_name, upstream, downstream, chromosome_list: pd.DataFrame, ignore_small_genes,
+                   output_name, model_case):
+    """
+    This function performs predictions, extracts correct predictions and performs shap computations. This will be
+    done iteratively per chromosome.
+
+    :param genome: genome fasta file
+    :param annot: gtf annotation file
+    :param tpm_targets: targets file; must have a target column
+    :param upstream: 1000
+    :param downstream: 500
+    :param n_chromosome: total number of chromosomes in the species
+    :param ignore_small_genes: whether to ignore small genes
+    :param output_name: prefix name used to create output files
+    :param model_case: SSR, SSC or MSR
+    :return: actual scores, hypothetical scores and one hot encodings of correct predictions across the entire genome
+    """
+    folder_path = make_absolute_path("results", "shap")
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    shap_actual_scores, shap_hypothetical_scores, one_hots_seqs, gene_ids_seqs, preds_seqs = [], [], [], [], []
+    loaded_input_files = load_input_files(genome_file_name=genome_file_name, annotation_file_name=annotation_file_name, tpm_counts_file_name=tpm_counts_file_name)
+    genome = loaded_input_files["genome"]
+    annotation = loaded_input_files["annotation"]
+    tpms = loaded_input_files["tpms"]
+    extracted_genes = extract_genes(genome, annotation, extragenic=upstream, intragenic=downstream, ignore_small_genes=ignore_small_genes, tpms=tpms, target_chromosomes=())
+    for val_chrom in chromosome_list:
+        x, y, preds, gene_ids, model = predict_self(extragenic=upstream, intragenic=downstream, val_chromosome=val_chrom,
+                                               output_name=output_name, model_case=model_case, extracted_genes=extracted_genes)
+        preds = preds > 0.5
+        preds = preds.astype(int)
+        correct_x, correct_y, correct_gene_ids = [], [], []
+        for idx in range(x.shape[0]): #type:ignore
+            if preds[idx] == y[idx]:
+                correct_x.append(x[idx])
+                correct_y.append(y[idx])
+                correct_gene_ids.append(gene_ids[idx])
+
+        correct_x = np.array(correct_x)
+
+        # Compute scores
+        print(f"Running shap for chromosome -----------------------------------------\n")
+        print(f"Chromosome: {val_chrom}: Species: {output_name}\n")
+        print(f"Running shap for chromosome -----------------------------------------\n")
+
+        actual_scores, hypothetical_scores = compute_actual_hypothetical_scores(x=correct_x, model=model)
+        shap_actual_scores.append(actual_scores)
+        shap_hypothetical_scores.append(hypothetical_scores)
+        one_hots_seqs.append(correct_x)
+        gene_ids_seqs.extend(correct_gene_ids)
+        preds_seqs.extend(correct_y)
+
+    shap_actual_scores = np.concatenate(shap_actual_scores, axis=0)
+    shap_hypothetical_scores = np.concatenate(shap_hypothetical_scores, axis=0)
+    one_hots_seqs = np.concatenate(one_hots_seqs, axis=0)
+
+    return shap_actual_scores, shap_hypothetical_scores, one_hots_seqs, gene_ids_seqs, preds_seqs
+
+
+def save_results(output_name: str, shap_actual_scores, shap_hypothetical_scores, gene_ids_seqs: List, preds_seqs: List):
+    folder_name = make_absolute_path("results", "shap")
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+    file_name = get_filename_from_path(__file__)
+    h5_file_name = os.path.join(folder_name, f'{output_name}_{file_name}_{get_time_stamp()}.h5')
+    with h5py.File(name=h5_file_name, mode='w') as h5_file:
+        h5_file.create_dataset(name='contrib_scores', data=shap_actual_scores)
+        h5_file.create_dataset(name="hypothetical_contrib_scores", data=shap_hypothetical_scores)
+        save_path = make_absolute_path('results', 'shap', f'{output_name}_{file_name}_{get_time_stamp()}_shap_meta.csv', start_file=__file__)
+        pd.DataFrame({'gene_ids': gene_ids_seqs, 'preds': preds_seqs}).to_csv(path_or_buf=save_path, index=False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                        prog='deepCRE',
+                        description="""
+                        This script performs the deepCRE prediction. We assume you have the following three directories:
+                        tmp_counts (contains your counts files), genome (contains the genome fasta files),
+                        gene_models (contains the gtf files)
+                        """)
+
+    parser.add_argument('--input',
+                        help="""
+                        This is a 5 column csv file with entries: genome, gtf, tpm, output name, number of chromosomes.""",
+                        required=True)
+    parser.add_argument('--model_case', help="Can be SSC or SSR", required=True)
+    parser.add_argument('--ignore_small_genes', help="Ignore small genes, can be yes or no", required=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    tf.compat.v1.disable_eager_execution()
+    tf.compat.v1.disable_v2_behavior()
+    tf.config.set_visible_devices([], 'GPU')
+
+    args = parse_args()
+    data = pd.read_csv(args.input, sep=',', header=None,
+                    dtype={0: str, 1: str, 2: str, 3: str, 4: str},
+                    names=['genome', 'gtf', 'tpm', 'output', 'chroms'])
+    print(data.head())
+    if data.shape[1] != 5:
+        raise Exception("Input file incorrect. Your input file must contain 5 columns and must be .csv")
+
+
+    for genome, gtf, tpm_counts, output_name, chromosomes_file in data.values:
+        chromosomes = pd.read_csv(filepath_or_buffer=f'genome/{chromosomes_file}', header=None).values.ravel().tolist()
+        results = extract_scores(genome_file_name=genome, annotation_file_name=gtf, tpm_counts_file_name=tpm_counts, upstream=1000, downstream=500,
+                    chromosome_list=chromosomes, ignore_small_genes=args.ignore_small_genes,
+                    output_name=output_name, model_case=args.model_case)
+        shap_actual_scores, shap_hypothetical_scores, one_hots_seqs, gene_ids_seqs, pred_seqs = results
+        save_results(shap_actual_scores=shap_actual_scores, shap_hypothetical_scores=shap_hypothetical_scores,
+                     output_name=output_name, gene_ids_seqs=gene_ids_seqs, preds_seqs=pred_seqs)
+
+if __name__ == "__main__":
+    main()
--- deepcre_predict.py
+++ deepcre_predict.py
@@ -1,46 +1,151 @@
 import argparse
 import os
-
+from typing import Any, Dict, List, Tuple
+import numpy as np
+from tensorflow.keras.models import load_model #type:ignore
 import pandas as pd
-from utils import predict
+import re
 
-if not os.path.exists('results/predictions'):
-    os.mkdir('results/predictions')
+from utils import get_filename_from_path, get_time_stamp, load_input_files, one_hot_encode, make_absolute_path, load_annotation
+from train_ssr_models import extract_genes
 
-parser = argparse.ArgumentParser(
-                    prog='deepCRE',
-                    description="""
-                    This script performs the deepCRE prediction. We assume you have the following three directories:
-                    tmp_counts (contains your counts files), genome (contains the genome fasta files),
-                    gene_models (contains the gtf files)
-                    """)
 
-parser.add_argument('--input',
-                    help="""
-                    This is a 5 column csv file with entries: genome, gtf, tpm, output name, number of chromosomes.""",
-                    required=True)
-parser.add_argument('--model_case', help="Can be SSC or SSR", required=True)
-parser.add_argument('--ignore_small_genes', help="Ignore small genes, can be yes or no", required=True)
+def find_newest_model_path(output_name: str, model_case: str, val_chromosome: str = "", model_path: str = "") -> Dict[str, str]:
+    """finds path to newest model fitting the given parameters
 
-args = parser.parse_args()
-data = pd.read_csv(args.input, sep=',', header=None,
-                   dtype={0: str, 1: str, 2: str, 3: str, 4: str},
-                   names=['genome', 'gtf', 'tpm', 'output', 'chroms'])
-print(data.head())
-if data.shape[1] != 5:
-    raise Exception("Input file incorrect. Your input file must contain 5 columns and must be .csv")
+    Args:
+        output_name (str): output name the was used for model training
+        val_chromosome (str): validation chromosome of the model. If it is not given, all models regardless of the val_chromosome will be returned
+        model_case (str): SSR or SSC for the model to be loaded
+        model_path (str): path to the directory where models are stored. used for testing, probably not really stable
 
-for genome, gtf, tpm_counts, output_name, num_chromosomes in data.values:
-    true_targets, preds, genes = [], [], []
-    chromosomes = pd.read_csv(filepath_or_buffer=f'genome/{num_chromosomes}', header=None).values.ravel().tolist()
-    for chrom in chromosomes:
-        _, y, pred_probs, gene_ids, _ = predict(genome=genome, annot=gtf, tpm_targets=tpm_counts, upstream=1000,
-                                                downstream=500, val_chromosome=str(chrom), output_name=output_name,
-                                                model_case=args.model_case, ignore_small_genes=args.ignore_small_genes)
-        true_targets.extend(y)
-        preds.extend(pred_probs)
-        genes.extend(gene_ids)
+    Raises:
+        ValueError: raises an error if no fitting model is found
 
-    result = pd.DataFrame({'true_targets': true_targets, 'pred_probs': preds, 'genes': genes})
-    print(result.head())
-    result.to_csv(f'results/predictions/{output_name}_predictions.csv', index=False)
+    Returns:
+        List[str]: List of path to the newest model fitting the given parameters for a single chromosome, or all fitting models if chromosome is ommitted.
+    """
+    if model_path == "":
+        path_to_models = make_absolute_path("saved_models", start_file=__file__)
+    else:
+        path_to_models = make_absolute_path(model_path, start_file=__file__)
+    # ^ and $ mark start and end of a string. \d singnifies any digit. \d+ means a sequence of digits with at least length 1
+    # more detailed explanation at https://regex101.com/, put in "^ara_(\d+)_ssr_\d+_\d+\.h5$"
+    if val_chromosome == "":
+        regex_string = f"^{output_name}_(.+)_{model_case}_train_ssr_models_\d+_\d+\.h5$"                                                                    #type:ignore
+    else:
+        regex_string = f"^{output_name}_{val_chromosome}_{model_case}_train_ssr_models_\d+_\d+\.h5$"                                                        #type:ignore
+    regex = re.compile(regex_string)
+    candidate_models = [model for model in os.listdir(path_to_models)]
+    fitting_models = {}
+    for candidate in candidate_models:
+        match = regex.match(candidate)
+        if match:
+            # group 1 is the "(.+)" part of the regex, so the place where the chromosome is located in the file name
+            chromosome = val_chromosome if val_chromosome else match.group(1)
+            if chromosome in fitting_models:
+                fitting_models[chromosome].append(candidate)
+            else:
+                fitting_models[chromosome] = [candidate]
+
+    if not fitting_models:
+        raise ValueError("no trained models fitting the given parameters were found! Consider training models first (train_ssr_models.py)")
+    for chromosome, models in fitting_models.items():
+        # models per chromosome only differ in the time stamp. So if sorted, the last model will be the most recently trained
+        models.sort()
+        fitting_models[chromosome] = os.path.join(path_to_models, models[-1])
+    return fitting_models
+
+
+def predict_self(extragenic, intragenic, val_chromosome, output_name, model_case, extracted_genes):
+    x, y, gene_ids = extracted_genes[str(val_chromosome)]
+
+    # Masking
+    x[:, extragenic:extragenic + 3, :] = 0                                                                                                  #type:ignore
+    x[:, extragenic + (intragenic * 2) + 17:extragenic + (intragenic * 2) + 20, :] = 0                                                      #type:ignore
+
+    newest_model_paths = find_newest_model_path(output_name=output_name, val_chromosome=val_chromosome, model_case=model_case)
+    model = load_model(newest_model_paths[val_chromosome])
+    pred_probs = model.predict(x).ravel()
+    return x, y, pred_probs, gene_ids, model
+
+def predict_other(extragenic, intragenic, val_chromosome, output_name, model_case, extracted_genes) -> Tuple[pd.DataFrame, Dict[str, Any]]:
+
+    x, y, gene_ids = extracted_genes[str(val_chromosome)]
+
+    # Masking
+    x[:, extragenic:extragenic + 3, :] = 0                                                                                                  #type:ignore
+    x[:, extragenic + (intragenic * 2) + 17:extragenic + (intragenic * 2) + 20, :] = 0                                                      #type:ignore
+
+    newest_model_paths = find_newest_model_path(output_name=output_name, model_case=model_case)
+    models = {os.path.basename(model_path): load_model(model_path) for chromosome, model_path in newest_model_paths.items()}
+
+
+    df_dict = {model_name: model.predict(x).ravel() for model_name, model in models.items()}
+    df_dict['true_targets'] = y
+    df_dict['genes'] = gene_ids
+    result_df = pd.DataFrame(df_dict)
+    return result_df, models
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+                        prog='deepCRE',
+                        description="""
+                        This script performs the deepCRE prediction. We assume you have the following three directories:
+                        tmp_counts (contains your counts files), genome (contains the genome fasta files),
+                        gene_models (contains the gtf files)
+                        """)
+
+    parser.add_argument('--input',
+                        help="""
+                        This is a 5 column csv file with entries: genome, gtf, tpm, output name, number of chromosomes.""",
+                        required=True)
+    parser.add_argument('--model_case', help="Can be SSC or SSR", required=True)
+    parser.add_argument('--ignore_small_genes', help="Ignore small genes, can be yes or no", required=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    data = pd.read_csv(args.input, sep=',', header=None,
+                    dtype={0: str, 1: str, 2: str, 3: str, 4: str},
+                    names=['genome', 'gtf', 'tpm', 'output', 'chroms'])
+    print(data.head())
+    if data.shape[1] != 5:
+        raise Exception("Input file incorrect. Your input file must contain 5 columns and must be .csv")
+
+    folder_name = make_absolute_path('results', 'predictions', start_file=__file__)
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    file_name = get_filename_from_path(__file__)
+
+
+    for genome_file_name, annotation_file_name, tpm_counts_file_name, output_name, chromosome_file in data.values:
+        true_targets, preds, genes = [], [], []
+        loaded_input_files = load_input_files(genome_file_name=genome_file_name, annotation_file_name=annotation_file_name, tpm_counts_file_name=tpm_counts_file_name)
+        genome = loaded_input_files["genome"]
+        annotation = loaded_input_files["annotation"]
+        tpms = loaded_input_files["tpms"]
+        extragenic = 1000
+        intragenic = 500
+        ignore_small_genes = args.ignore_small_genes
+        extracted_genes = extract_genes(genome=genome, annotation=annotation, extragenic=extragenic, intragenic=intragenic, ignore_small_genes=ignore_small_genes, tpms=tpms, target_chromosomes=())
+        chromosomes = pd.read_csv(filepath_or_buffer=f'genome/{chromosome_file}', header=None).values.ravel().tolist()
+        for chrom in chromosomes:
+            _, y, pred_probs, gene_ids, _ = predict_self(extragenic=extragenic, intragenic=intragenic, val_chromosome=str(chrom), output_name=output_name,
+                                                    model_case=args.model_case, extracted_genes=extracted_genes)
+            true_targets.extend(y)
+            preds.extend(pred_probs)
+            genes.extend(gene_ids)
+
+        result = pd.DataFrame({'true_targets': true_targets, 'pred_probs': preds, 'genes': genes})
+        print(result.head())
+        output_location = os.path.join(folder_name, f'{output_name}_{file_name}_{get_time_stamp()}.csv')
+        result.to_csv(output_location, index=False)
+
+
+if __name__ == "__main__":
+    main()
