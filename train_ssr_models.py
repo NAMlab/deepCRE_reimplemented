c7236de09ce9e9bfe16678a6d7cc75a6c538d297
Gernot Schmitz <spammail1240@web.de>
2024-09-16 16:52:08 +0200

    summary of errors will now only be shown when errors actually occur. also removed randomly created folders outside of funtions

--- train_ssr_models.py
+++ train_ssr_models.py
@@ -253,24 +253,17 @@ def train_deep_cre(genome, annot, tpm_targets, upstream, downstream, genes_picke
 
 
 def result_summary(failed_trainings: List[Tuple[str, int, Exception]], passed_trainings: List[Tuple[str, int]], input_length: int) -> None:
-    print("_______________________________________________________________")
-    print(f"During training the following errors occurred:")
-    for name, line, err in failed_trainings:
-        print(f"output name \"{name}\" (line {line + 1} in the input file) failed with error message:\n{err}")
+    if failed_trainings:
         print("_______________________________________________________________")
-    print(f"{len(passed_trainings)} / {input_length} passed.")
-    print("_______________________________________________________________")
-    print(f"names of the failed runs:", end=" ")
-    for name, line, _ in failed_trainings:
-        print(f"{name} (line {line + 1})", sep=", ")
-
-
-os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
-
-if not os.path.exists('results'):
-    os.makedirs('results')
-if not os.path.exists('saved_models'):
-    os.makedirs('saved_models')
+        print(f"During training the following errors occurred:")
+        for name, line, err in failed_trainings:
+            print(f"output name \"{name}\" (line {line + 1} in the input file) failed with error message:\n{err}")
+            print("_______________________________________________________________")
+        print(f"{len(passed_trainings)} / {input_length} passed.")
+        print("_______________________________________________________________")
+        print(f"names of the failed runs:", end=" ")
+        for name, line, _ in failed_trainings:
+            print(f"{name} (line {line + 1})", sep=", ")
 
 
 def parse_args():
