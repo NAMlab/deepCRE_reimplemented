f3092156d6f15984f1ce1477c9e2ba31678bdb0a
Gernot Schmitz <spammail1240@web.de>
2024-10-18 15:11:27 +0200

    added try-catch to deepcre_predict main loop

--- deepcre_predict.py
+++ deepcre_predict.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import load_model #type:ignore
 import pandas as pd
 import re
 
-from utils import get_filename_from_path, get_time_stamp, load_input_files, one_hot_encode, make_absolute_path
+from utils import get_filename_from_path, get_time_stamp, load_input_files, one_hot_encode, make_absolute_path, result_summary
 from train_ssr_models import extract_genes
 
 
@@ -99,29 +99,35 @@ def main():
         os.makedirs(folder_name)
     file_name = get_filename_from_path(__file__)
 
+    failed_trainings = []
+    for i, (genome_file_name, annotation_file_name, tpm_counts_file_name, output_name, chromosome_file) in enumerate(data.values):
+        try:
+            true_targets, preds, genes = [], [], []
+            loaded_input_files = load_input_files(genome_file_name=genome_file_name, annotation_file_name=annotation_file_name, tpm_counts_file_name=tpm_counts_file_name)
+            genome = loaded_input_files["genome"]
+            annotation = loaded_input_files["annotation"]
+            tpms = loaded_input_files["tpms"]
+            extragenic = 1000
+            intragenic = 500
+            ignore_small_genes = args.ignore_small_genes.lower() == "yes"
+            extracted_genes = extract_genes(genome=genome, annotation=annotation, extragenic=extragenic, intragenic=intragenic, ignore_small_genes=ignore_small_genes, tpms=tpms, target_chromosomes=())
+            chromosomes = pd.read_csv(filepath_or_buffer=f'genome/{chromosome_file}', header=None).values.ravel().tolist()
+            for chrom in chromosomes:
+                _, y, pred_probs, gene_ids, _ = predict_self(extragenic=extragenic, intragenic=intragenic, val_chromosome=str(chrom), output_name=output_name,
+                                                        model_case=args.model_case, extracted_genes=extracted_genes)
+                true_targets.extend(y)
+                preds.extend(pred_probs)
+                genes.extend(gene_ids)
 
-    for genome_file_name, annotation_file_name, tpm_counts_file_name, output_name, chromosome_file in data.values:
-        true_targets, preds, genes = [], [], []
-        loaded_input_files = load_input_files(genome_file_name=genome_file_name, annotation_file_name=annotation_file_name, tpm_counts_file_name=tpm_counts_file_name)
-        genome = loaded_input_files["genome"]
-        annotation = loaded_input_files["annotation"]
-        tpms = loaded_input_files["tpms"]
-        extragenic = 1000
-        intragenic = 500
-        ignore_small_genes = args.ignore_small_genes.lower() == "yes"
-        extracted_genes = extract_genes(genome=genome, annotation=annotation, extragenic=extragenic, intragenic=intragenic, ignore_small_genes=ignore_small_genes, tpms=tpms, target_chromosomes=())
-        chromosomes = pd.read_csv(filepath_or_buffer=f'genome/{chromosome_file}', header=None).values.ravel().tolist()
-        for chrom in chromosomes:
-            _, y, pred_probs, gene_ids, _ = predict_self(extragenic=extragenic, intragenic=intragenic, val_chromosome=str(chrom), output_name=output_name,
-                                                    model_case=args.model_case, extracted_genes=extracted_genes)
-            true_targets.extend(y)
-            preds.extend(pred_probs)
-            genes.extend(gene_ids)
+            result = pd.DataFrame({'true_targets': true_targets, 'pred_probs': preds, 'genes': genes})
+            print(result.head())
+            output_location = os.path.join(folder_name, f'{output_name}_{file_name}_{get_time_stamp()}.csv')
+            result.to_csv(output_location, index=False)
+        except Exception as e:
+            print(e)
+            failed_trainings.append((output_name, i, e))
 
-        result = pd.DataFrame({'true_targets': true_targets, 'pred_probs': preds, 'genes': genes})
-        print(result.head())
-        output_location = os.path.join(folder_name, f'{output_name}_{file_name}_{get_time_stamp()}.csv')
-        result.to_csv(output_location, index=False)
+    result_summary(failed_trainings=failed_trainings, input_length=len(data), script=get_filename_from_path(__file__))
 
 
 if __name__ == "__main__":
