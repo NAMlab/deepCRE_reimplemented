f737ecccf75503859c21ef7b0abf8d0877f28171
Gernot Schmitz <spammail1240@web.de>
2024-11-05 14:29:47 +0100

    made executable from command line

--- create_target_file.py
+++ create_target_file.py
@@ -1,11 +1,27 @@
+import argparse
+import os
 import pandas as pd
 import numpy as np
 from utils import make_absolute_path
 
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Convert RNA seq data into targets for training.")
+    parser.add_argument("--input_path", "-i", help="path to the RNA seq data.", required=True)
+    parser.add_argument("--output_path", "-o", help="path to output file.", default="")
+    args = parser.parse_args()
+    return args
+
+
 def main():
-    tpm_path = make_absolute_path("tpm_couns", "arabidopsis_counts.csv", start_file=__file__)
-    output_path = make_absolute_path("tpm_couns", "arabidopsis_targets.csv", start_file=__file__)
+    args = parse_args()
+    tpm_path = args.input_path
+    if args.output_path:
+        output_path = args.output_path
+    else:
+        base_name = os.path.splitext(os.path.basename(tpm_path))[0]
+        output_path = make_absolute_path("tpm_counts", f"{base_name}_targets.csv", start_file=__file__)
 
     tpm_counts = pd.read_csv(tpm_path)
     true_targets = []
