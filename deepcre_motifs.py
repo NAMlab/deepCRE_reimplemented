89a41fa7e7d946206d134ff88a49d4e358f9aa6e
Gernot Schmitz <spammail1240@web.de>
2025-01-07 16:37:35 +0100

    deepcre_motifs runs with json inputs now. fixed logic on force_interpretation in generate_motifs. added test case for motif extraction

--- deepcre_motifs.py
+++ deepcre_motifs.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import List, Optional, Tuple
 import pandas as pd
 import tensorflow as tf
 import os
@@ -7,6 +8,7 @@ from importlib import reload
 import h5py
 from utils import get_filename_from_path, get_time_stamp, make_absolute_path, load_annotation_msr, result_summary
 from deepcre_interpret import extract_scores, find_newest_interpretation_results
+from parsing import ModelCase, ParsedInputs, RunInfo
 
 
 def modisco_run(contribution_scores, hypothetical_scores, one_hots, output_name):
@@ -49,23 +51,27 @@ def modisco_run(contribution_scores, hypothetical_scores, one_hots, output_name)
 
 
 def generate_motifs(genome, annot, tpm_targets, upstream, downstream, ignore_small_genes,
-                    output_name, model_case, chromosome_list: pd.DataFrame, train_val_split, force_interpretation: bool = False):
-    try:
-        if force_interpretation:
-            raise ValueError()
-        saved_interpretation_results_path = find_newest_interpretation_results(output_name=output_name, results_path=os.path.join("results", "shap"))
-        with h5py.File(saved_interpretation_results_path, "r") as f:
-            actual_scores = f["contrib_scores"][:] #type:ignore
-            hypothetical_scores = f["hypothetical_contrib_scores"][:] #type:ignore
-            one_hots = f["one_hot_seqs"][:] #type:ignore
-    except ValueError:
+                    output_name, model_case, chromosome_list: Optional[List[str]], force_interpretation: bool = False):
+    #just load existing scores
+    if not force_interpretation:
+        try: 
+            saved_interpretation_results_path = find_newest_interpretation_results(output_name=output_name, results_path=os.path.join("results", "shap"))
+            with h5py.File(saved_interpretation_results_path, "r") as f:
+                actual_scores = f["contrib_scores"][:] #type:ignore
+                hypothetical_scores = f["hypothetical_contrib_scores"][:] #type:ignore
+                one_hots = f["one_hot_seqs"][:] #type:ignore
+        except ValueError as e:
+            force_interpretation = True
+    # recalculate the scores if the user wants to force the interpretation or if the scores were not found
+    if force_interpretation:
         actual_scores, hypothetical_scores, one_hots, _, _ = extract_scores(genome_file_name=genome, annotation_file_name=annot,
                                                                             tpm_counts_file_name=tpm_targets,
                                                                             upstream=upstream, downstream=downstream,
                                                                             chromosome_list=chromosome_list,
                                                                             ignore_small_genes=ignore_small_genes,
                                                                             output_name=output_name,
-                                                                            model_case=model_case, train_val_split=train_val_split)
+                                                                            model_case=model_case)
+        
 
     print("Now running MoDisco --------------------------------------------------\n")
     print(f"Species: {output_name} \n")
@@ -79,75 +85,67 @@ def parse_args():
                         description="This script performs the deepCRE prediction. We assume you have the following three" + 
                         "directories:tmp_counts (contains your counts files), genome (contains the genome fasta files), gene_models (contains the gtf files)")
 
-    parser.add_argument('--input', "-i", "-i", 
-                        help="""For model case SSR/SSC: This is a six column csv file with entries: species, genome, gtf, tpm, output name, number of chromosomes and pickle_key. \n 
-                        For model case MSR: This is a five column csv file with entries: species, genome, gtf, tpm, output name.""", required=True)
-    parser.add_argument('--model_case', "-mc", help="Can be SSC, SSR or MSR", required=True, choices=["msr", "ssr", "ssc", "both"])
-    parser.add_argument('--ignore_small_genes', "-isg", help="Ignore small genes, can be yes or no", required=False, choices=["yes", "no"], default="yes")
-    parser.add_argument('--force_interpretations', "-fi", help="determines whether interpretations are recalculated, even if there are saved interpretations. If false (default), saved values will be used.", required=False, default="false", choices=["true", "false"])
-    parser.add_argument('--train_val_split', help="For SSR /SSC training: Creates a training/validation dataset with 80%/20% of genes, can be yes or no", required=False, choices=["yes", "no"], default="no")
+    parser.add_argument('--input', "-i",
+                        help="""json file containing the required input parameters. Possible arguments can be seen in the file parsing.py in the two global dictionaries.
+                        Example file is inputs.json.""", required=True)
 
     args = parser.parse_args()
     return args
 
 
-def main():
+def run_motif_extraction(inputs: ParsedInputs, failed_trainings: List[Tuple], input_length: int):
     tf.compat.v1.disable_eager_execution()
     tf.compat.v1.disable_v2_behavior()
     tf.config.set_visible_devices([], 'GPU')
+
+    run_info: RunInfo
+    for i, run_info in enumerate(inputs):       #type:ignore
+        try:
+            if run_info.general_info["model_case"] == ModelCase.MSR:
+                # train_specie = data.copy()
+                # train_specie = train_specie[train_specie['specie'] != specie]
+
+                # output_name = "_".join([sp[:3].lower() for sp in train_specie['specie'].unique()])
+                # chromosomes=""
+                output_name = run_info.species_info[0]["subject_species"]
+
+            elif run_info.general_info["model_case"] in [ModelCase.SSR, ModelCase.SSC]:
+                output_name = run_info.general_info["training_output_name"]
+            generate_motifs(genome=run_info.general_info["genome"], annot=run_info.general_info["annotation"], tpm_targets=run_info.general_info["targets"],
+                            upstream=run_info.general_info["extragenic"], downstream=run_info.general_info["intragenic"],
+                            ignore_small_genes=run_info.general_info["ignore_small_genes"], output_name=output_name,
+                            model_case=run_info.general_info["model_case"], chromosome_list=run_info.general_info["chromosomes"], force_interpretation=run_info.general_info["force_interpretations"])
+        except Exception as e:
+            print(e)
+            failed_trainings.append((output_name, i, e))
+
+    result_summary(failed_trainings=failed_trainings, input_length=input_length, script=get_filename_from_path(__file__))
+
+
+def main():
+    possible_general_parameters = {
+        "model_case": None,
+        "genome": None,
+        "annotation": None,
+        "targets": None,
+        "training_output_name": "",
+        "chromosomes": "",
+        "ignore_small_genes": True,
+        "subject_species": "",
+        "extragenic": 1000,
+        "intragenic": 500,
+        "force_interpretations": False
+    }
+
+    possible_species_parameters = {
+        "subject_species": "",
+    }
     args = parse_args()
-    force_interpretation = args.force_interpretations == "true"
-    model_case = args.model_case 
+    inputs, failed_trainings, input_length = ParsedInputs.parse(args.input, possible_general_parameters=possible_general_parameters, possible_species_parameters=possible_species_parameters)
+    inputs = inputs.replace_both()
+    print(inputs)
 
-
-    dtypes = {0: str, 1: str, 2: str, 3: str, 4: str}
-    names = ['specie','genome', 'gtf', 'tpm', 'output', "chroms", "p_key"] if model_case.lower() == "msr" else ['genome', 'gtf', 'tpm', 'output', 'chroms']
-    data = pd.read_csv(args.input, sep=',', header=None, dtype=dtypes, names = names)
-    expected_columns = len(names)
-
-    print(data.head())
-    ignore_small_genes_flag = args.ignore_small_genes.lower() == "yes"
-    
-    if model_case.lower() == "msr":
-        p_keys = "_".join(data['p_key'].unique())
-        input_filename = args.input.split('.')[0] 
-
-        genome_path = make_absolute_path("genome", f"genome_{p_keys}.fa", start_file=__file__)     
-        tpm_path = make_absolute_path("tpm_counts", f"tpm_{p_keys}_{input_filename}.csv", start_file=__file__)  # tpm_targets = f"tpm_{p_keys}.csv"
-        annotation_path = make_absolute_path("gene_models", f"gtf_{p_keys}.csv", start_file=__file__)  
-        annotation = load_annotation_msr(annotation_path)
-
-
-        for specie in data['specie'].unique():                                                                     
-            test_specie = data.copy()
-            test_specie = test_specie[test_specie['specie'] == specie]
-            train_specie = data.copy()
-            train_specie = train_specie[train_specie['specie'] != specie]
-
-            output_name = "_".join([sp[:3].lower() for sp in train_specie['specie'].unique()])
-            
-            test_specie_name = test_specie['specie'].values[0]
-            #chromosomes = annotation[annotation['species'] == test_specie_name]['Chromosome'].unique().tolist()
-            #chromosomes = sorted(chromosomes, key=lambda x: int("".join(filter(str.isdigit, x)))) 
-            chromosomes=""
-
-            generate_motifs(genome=genome_path, annot=annotation_path, tpm_targets=tpm_path, upstream=1000, downstream=500,
-                            ignore_small_genes=ignore_small_genes_flag, output_name=output_name,
-                            model_case=args.model_case, chromosome_list=chromosomes, train_val_split=args.train_val_split)
-
-    elif model_case.lower() in ["ssr", "ssc"]:
-        failed_trainings = []
-        for i, (genome, gtf, tpm_counts, output_name, chromosomes_file) in enumerate(data.values):
-            try:
-                chromosomes = pd.read_csv(filepath_or_buffer=f'genome/{chromosomes_file}', header=None).values.ravel().tolist()
-                generate_motifs(genome=genome, annot=gtf, tpm_targets=tpm_counts, upstream=1000, downstream=500,
-                                ignore_small_genes=ignore_small_genes_flag, output_name=output_name,
-                                model_case=args.model_case, chromosome_list=chromosomes, train_val_split=args.train_val_split, force_interpretation=force_interpretation)
-            except Exception as e:
-                print(e)
-                failed_trainings.append((output_name, i, e))
-
-        result_summary(failed_trainings=failed_trainings, input_length=len(data), script=get_filename_from_path(__file__))
+    run_motif_extraction(inputs, failed_trainings, input_length)
 
 
 if __name__ == "__main__":
