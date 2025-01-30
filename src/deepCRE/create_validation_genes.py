434bf5dbfe501fa4172ee38a5e026b888db2f5d1
Gernot Schmitz <spammail1240@web.de>
2025-01-30 16:44:06 +0100

    updated pickel file creation, and added command line parsing

--- src/deepCRE/create_validation_genes.py
+++ src/deepCRE/create_validation_genes.py
@@ -2,43 +2,106 @@ import os
 import pickle
 import pandas as pd
 from Bio import SeqIO
+import re
+import argparse
+from deepCRE.utils import make_absolute_path
 
-proteins = ['Arabidopsis_thaliana.TAIR10.pep.all.fa', 'Zea_mays.Zm-B73-REFERENCE-NAM-5.0.pep.all.fa',
-            'Solanum_lycopersicum.SL3.0.pep.all.fa', 'Sorghum_bicolor.Sorghum_bicolor_NCBIv3.pep.all.fa']
 
-blast_outputs = ['BLAST_ara_to_ara', 'BLAST_zea_to_zea', 'BLAST_sol_to_sol', 'BLAST_sor_to_sor']
-validation_genes = dict()
-for proteome, blast_output in zip(proteins, blast_outputs):
-    if os.path.exists(f'proteomes/{blast_output}'):
+def parse_args():
+    parser = argparse.ArgumentParser(
+                        prog='create validation genes',
+                        description="This script can be used to create a list of validation genes for a given proteome.")
+    parser.add_argument('--proteins', "-p", help="Path to Proteome file", required=True)
+    parser.add_argument('--blast_outputs', "-b", help="Path to BLAST output file. Needs to be in outfmt 6", required=True)
+    parser.add_argument('--pickle_key_name', "-k", help="Key for the pickle dictionary", required=True)
+    parser.add_argument('--output', "-o", help="Path to an output file. By default will create a file in the src/deepCRE folder.", required=False, default="")
+
+    args = parser.parse_args()
+    return args
+
+
+def get_save_path(output_path: str, pickle_key: str):
+    if not output_path:
+        pickle_file_path = make_absolute_path(f"validation_genes_{pickle_key}.pickle", start_file=__file__)
+
+    # Remove any existing pickle file
+    if os.path.exists(pickle_file_path):
+        raise FileExistsError(f"File already exists at {pickle_file_path}. Please remove it or provide a different output path.")
+    return pickle_file_path
+
+
+def main():
+    args = parse_args()
+    proteome_path = args.proteins
+    blast_output_path = args.blast_outputs
+    pickle_key_name = args.pickle_key_name # Key for the pickle dictionary
+    output_path = args.output
+    print(proteome_path, blast_output_path, pickle_key_name, output_path)
+
+    pickle_file_path = get_save_path(output_path=output_path, pickle_key=pickle_key_name)
+
+    # Dictionary to store validation genes
+    validation_genes = {}
+
+    if os.path.exists(blast_output_path):
+        # Parse protein sequences and build a DataFrame
         info = []
-        for rec in SeqIO.parse(f'proteomes/{proteome}', 'fasta'):
-            description = rec.description.split(' ')
-            protein_id = description[0]
-            gene_id = description[3].split(':')[-1]
-            chrom = description[2].split(':')[2]
+        for rec in SeqIO.parse(proteome_path, 'fasta'):
+            description = rec.description
+            protein_id = description.split(' ')[0]  # Extract protein_id
+            gene_match = re.search(r'gene=([^ ]+)', description)
+            chrom_match = re.search(r'seq_id=([^ ]+)', description)
+            gene_id = gene_match.group(1) if gene_match else None
+            chrom = chrom_match.group(1) if chrom_match else None
             info.append([gene_id, protein_id, chrom])
-        info = pd.DataFrame(info, columns=['gene_id', 'protein_id', 'chrom'])
-        info.index = info.protein_id.tolist()
-        print(info.head())
-        blast_out = pd.read_csv(f'proteomes/{blast_output}', sep='\t',
-                                names=['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstat', 'qend',
-                                       'sstart', 'send', 'evalue', 'bitscore'])
-        blast_out['qgene'] = info.loc[blast_out.qseqid.tolist(), 'gene_id'].values
-        blast_out['sgene'] = info.loc[blast_out.sseqid.tolist(), 'gene_id'].values
-        blast_out['qchrom'] = info.loc[blast_out.qseqid.tolist(), 'chrom'].values
-        blast_out['schrom'] = info.loc[blast_out.sseqid.tolist(), 'chrom'].values
-        blast_out = blast_out[blast_out['evalue'] < 0.001]
-        blast_out = blast_out[blast_out['bitscore'] >= 50]
-        blast_out = blast_out[~blast_out['schrom'].isin(['Pt', 'Mt'])]
-        blast_out = blast_out[~blast_out['qchrom'].isin(['Pt', 'Mt'])]
-        blast_out['homologs_pairs'] = [x + '_' + y for x, y in zip(blast_out.qchrom, blast_out.schrom)]
-        val_set = []
-        for gene_grp in blast_out.groupby('qgene'):
-            if len(gene_grp[1].homologs_pairs.unique()) == 1:
-                val_set.append(gene_grp[0])
-        validation_genes[f"{blast_output.split('_')[-1]}"] = val_set
 
-if os.path.exists('../model/validation_genes.pickle'):
-    os.remove('../model/validation_genes.pickle')
-with open('../model/validation_genes.pickle', 'wb') as pickle_file:
-    pickle.dump(validation_genes, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
+        info = pd.DataFrame(info, columns=['gene_id', 'protein_id', 'chrom'])
+        info.index = info.protein_id.tolist()  #type:ignore
+
+        # Read BLAST output
+        blast_out = pd.read_csv(
+            blast_output_path,
+            sep='\t',
+            names=[
+                'qseqid', 'sseqid', 'pident', 'length', 'mismatch',
+                'gap_open', 'qstart', 'qend', 'sstart', 'send', 'evalue',
+                'bitscore'
+            ]
+        )
+
+        # Filter BLAST output for valid protein IDs
+        valid_ids = [seq_id for seq_id in blast_out.qseqid.tolist() if seq_id in info.index]
+        blast_out = blast_out[blast_out['qseqid'].isin(valid_ids)]
+
+        if not blast_out.empty:
+            # Map gene and chromosome information
+            blast_out['qgene'] = info.loc[blast_out.qseqid.tolist(), 'gene_id'].values
+            blast_out['sgene'] = info.loc[blast_out.sseqid.tolist(), 'gene_id'].values
+            blast_out['qchrom'] = info.loc[blast_out.qseqid.tolist(), 'chrom'].values
+            blast_out['schrom'] = info.loc[blast_out.sseqid.tolist(), 'chrom'].values
+
+            # Apply filtering criteria
+            blast_out = blast_out[blast_out['evalue'] < 0.001]
+            blast_out = blast_out[blast_out['bitscore'] >= 50]
+            blast_out = blast_out[~blast_out['schrom'].isin(['Pt', 'Mt'])]
+            blast_out = blast_out[~blast_out['qchrom'].isin(['Pt', 'Mt'])]
+
+            # Identify validation genes
+            val_set = []
+            for gene_grp in blast_out.groupby('qgene'):
+                if len(gene_grp[1]['qchrom'].unique()) == 1:
+                    val_set.append(gene_grp[0])
+
+            # Save flat list of validation genes for this output
+            validation_genes[pickle_key_name] = val_set
+    else:
+        print(f"BLAST output file not found at {blast_output_path}")
+
+    # Save the dictionary with the flattened list of gene IDs
+    with open(pickle_file_path, 'wb') as pickle_file:
+        pickle.dump(validation_genes, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print(f"Validation genes saved to {pickle_file_path}")
+
+if __name__ == "__main__":
+    main()
